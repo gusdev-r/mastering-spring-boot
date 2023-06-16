f862f539983501c34aefe9e6798b3ea85d5c3cf1
gusdev-r <g.hen.moreira@gmail.com>
2023-06-16 00:33:38 -0300

    initial commit, project generation

---
+++ src/test/java/academy/devgus/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devgus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
