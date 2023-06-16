d62b2ff77f52fedf98560893fa949d6a3470a87b
gusdev-r <g.hen.moreira@gmail.com>
2023-06-16 11:32:15 -0300

    Exercise - List of Anime - In this class the exercise was created a controller with a method to return a list of anime with the possibilities URL: v1/animes and v1/animes/

---
+++ src/main/java/academy/devgus/demo/util/DateUtil.java
@@ -0,0 +1,13 @@
+package academy.devgus.demo.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component //bean
+public class DateUtil {
+    public String formatLocalDateTimeToDataBaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+}
